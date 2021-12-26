f6a7798301464a5f7c9a9620f2c1db671cf3443b
xencodes <xencodes@icloud.com>
2021-12-26 13:53:25 +0530

    add title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,10 +9,14 @@ import '@fontsource/open-sans/400.css'
 import type { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
 import theme from '../theme'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
+      <Head>
+        <title>remac</title>
+      </Head>
       <Component {...pageProps} />
     </ChakraProvider>
   )
